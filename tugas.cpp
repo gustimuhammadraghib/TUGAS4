2d1787b80671f529caf275dc1444e8905bdd3388
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-01-19 01:37:35 +0800

    memasukkan nilai program c++

--- tugas.cpp
+++ tugas.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+class pakaian {
+    public:
+        void model (string pakaian);
+        string jenisbahan;
+        void contoh (string berbentuk);
+    
+                
+        };
+int main(){
+	
+    pakaian CASUAL,FORMAL,ETNIS; 
+
+	CASUAL.jenisbahan					="Bahan Kain 	: Katun";
+	ETNIS.jenisbahan					="Bahan Kain 	: Rajut";
+	FORMAL.jenisbahan					="Bahan Kain  	: Sifon";
+}
